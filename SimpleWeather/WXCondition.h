c3b966da6be4696f46f1c6fceb556e26f379fd50
Masayoshi Sekimura <sekimura@gmail.com>
2014-12-21 18:13:12 -0800

    Step 5-2
    Creating Your Weather Model
    
    Your models will use Mantle which makes data mapping and value
    transformation a cinch.
    
    The MTLJSONSerializing protocol tells the Mantle serializer that this
    object has instructions on how to map JSON to Objective-C properties.
    
    Properties are all of your weather data properties. You’ll be using a
    couple of them, but its nice to have access to all of the data in the
    event that you want to extend your app down the road.
    
    imageName is simply a helper method to map weather conditions to imageName
    files.

--- SimpleWeather/WXCondition.h
+++ SimpleWeather/WXCondition.h
@@ -6,8 +6,24 @@
 //  Copyright (c) 2014 Magic Symmetry. All rights reserved.
 //
 
-#import "MTLModel.h"
+#import <Mantle.h>
 
-@interface WXCondition : MTLModel
+@interface WXCondition : MTLModel <MTLJSONSerializing>
+
+@property (nonatomic, strong) NSDate *date;
+@property (nonatomic, strong) NSNumber *humidity;
+@property (nonatomic, strong) NSNumber *temperature;
+@property (nonatomic, strong) NSNumber *tempHigh;
+@property (nonatomic, strong) NSNumber *tempLow;
+@property (nonatomic, strong) NSString *locationName;
+@property (nonatomic, strong) NSDate *sunrise;
+@property (nonatomic, strong) NSDate *sunset;
+@property (nonatomic, strong) NSString *conditionDescription;
+@property (nonatomic, strong) NSString *condition;
+@property (nonatomic, strong) NSNumber *windBearing;
+@property (nonatomic, strong) NSNumber *windSpeed;
+@property (nonatomic, strong) NSString *icon;
+
+- (NSString *)imageName;
 
 @end
