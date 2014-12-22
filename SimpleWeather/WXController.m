ed00e260c06a08d1fed7ec1b2f3da96eae669de5
Masayoshi Sekimura <sekimura@gmail.com>
2014-12-21 18:04:47 -0800

    Step 4-11
    Do the heavy lifting of setting up the various controls in your view.
    
    * Set the current-conditions view as your table header.
    * Build each required label to display weather data.
    * Add an image view for a weather icon.

--- SimpleWeather/WXController.m
+++ SimpleWeather/WXController.m
@@ -87,6 +87,51 @@
   CGRect conditionsFrame = iconFrame;
   conditionsFrame.size.width = self.view.bounds.size.width - (((2 * inset) + iconHeight) + 10);
   conditionsFrame.origin.x = iconFrame.origin.x + (iconHeight + 10);
+
+  // Set the current-conditions view as your table header.
+  UIView *header = [[UIView alloc] initWithFrame:headerFrame];
+  header.backgroundColor = [UIColor clearColor];
+  self.tableView.tableHeaderView = header;
+  
+  // Build each required label to display weather data.
+  // bottom left. Big one.
+  UILabel *temperatureLabel = [[UILabel alloc] initWithFrame:temperatureFrame];
+  temperatureLabel.backgroundColor = [UIColor clearColor];
+  temperatureLabel.textColor = [UIColor whiteColor];
+  temperatureLabel.text = @"0°";
+  temperatureLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:120];
+  [header addSubview:temperatureLabel];
+  
+  // bottom left. Small one under tempperatureLabel
+  UILabel *hiloLabel = [[UILabel alloc] initWithFrame:hiloFrame];
+  hiloLabel.backgroundColor = [UIColor clearColor];
+  hiloLabel.textColor = [UIColor whiteColor];
+  hiloLabel.text = @"0° / 0°";
+  hiloLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:28];
+  [header addSubview:hiloLabel];
+  
+  // top
+  UILabel *cityLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, self.view.bounds.size.width, 30)];
+  cityLabel.backgroundColor = [UIColor clearColor];
+  cityLabel.textColor = [UIColor whiteColor];
+  cityLabel.text = @"Loading...";
+  cityLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18];
+  cityLabel.textAlignment = NSTextAlignmentCenter;
+  [header addSubview:cityLabel];
+  
+  // bottom left. Next to iconView
+  UILabel *conditionsLabel = [[UILabel alloc] initWithFrame:conditionsFrame];
+  conditionsLabel.backgroundColor = [UIColor clearColor];
+  conditionsLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18];
+  conditionsLabel.textColor = [UIColor whiteColor];
+  [header addSubview:conditionsLabel];
+  
+  // Add an image view for a weather icon.
+  // bottom left. On the top of temperatureLabel
+  UIImageView *iconView = [[UIImageView alloc] initWithFrame:iconFrame];
+  iconView.contentMode = UIViewContentModeScaleAspectFit;
+  iconView.backgroundColor = [UIColor clearColor];
+  [header addSubview:iconView];
 }
 
 - (void)viewWillLayoutSubviews {
