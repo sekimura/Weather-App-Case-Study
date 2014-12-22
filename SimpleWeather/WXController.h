4c3656ef527722cc833d29bbfddc6720d115ff97
Masayoshi Sekimura <sekimura@gmail.com>
2014-12-21 16:15:35 -0800

    Step 3-1
    Although the app looks complex, it will be powered by a single view
    controller. You’ll add that now.
    
    With the SimpleWeather project selected, click File\New\File and select
    Cocoa Touch\Objective-C class. Name your class WXController and make it
    a subclass of UIViewController.
    
    Make sure that both Targeted for iPad and With XIB for user interface
    are both unchecked.

---
+++ SimpleWeather/WXController.h
@@ -0,0 +1,13 @@
+//
+//  WXController.h
+//  SimpleWeather
+//
+//  Created by Masayoshi Sekimura on 12/21/14.
+//  Copyright (c) 2014 Magic Symmetry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXController : UIViewController
+
+@end
---
+++ SimpleWeather/WXController.m
@@ -0,0 +1,37 @@
+//
+//  WXController.m
+//  SimpleWeather
+//
+//  Created by Masayoshi Sekimura on 12/21/14.
+//  Copyright (c) 2014 Magic Symmetry. All rights reserved.
+//
+
+#import "WXController.h"
+
+@interface WXController ()
+
+@end
+
+@implementation WXController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
